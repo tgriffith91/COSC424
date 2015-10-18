0dac2fb1f5f5ac087a3b43f73b2d7b5866e0e95d
Thomas Griffith <mbjr@iup.edu>
2015-10-18 11:48:54 -0400

    Replace Original
    Tired to fix the problem with jre and eclipse

---
+++ Compiler Project 1/src/LexicalAnalyzer.java
@@ -0,0 +1,37 @@
+import java.nio.Buffer;
+import java.util.Scanner;
+
+/*
+ * @author Mary Kolojejchick 
+ * @author Tom Griffith
+ * 
+ * A lexical analyzer - part of the compiler.
+ */
+public class LexicalAnalyzer {
+	
+	static String readFile(){
+		//get filename from user
+	Scanner in = new Scanner(System.in);
+	System.out.print("Enter a file name:  ");
+	String fileName = in.next();
+	System.out.println();
+	
+	
+	String buffer = null;
+	return buffer;
+	}
+	
+	
+	//return token for next identified lexeme
+	private Token nextToken(){
+		
+		
+		return null;
+		
+	}
+	
+	
+	
+	
+	
+}
