028d0bc9655e9c6a528228a5060badd85789b7c3
Thomas Griffith <mbjr@iup.edu>
2015-10-18 11:45:07 -0400

    Revert "Compiler Construction"
    This reverts commit 9729835e78a3fda15359d0934182347b9dfd8766.

--- Compiler Project 1/src/Constant.java
+++
@@ -1,8 +0,0 @@
-
-public class Constant {
-	public static final Token t = new Token("if", "keyword", 900);
-	public static final Token t1 = new Token("int", "keyword", 901);
-	public static final Token t2 = new Token("double", "keyword", 902);
-	public static final Token t3 = new Token("else", "keyword", 903);
-	public static final Token t4 = new Token("while", "keyword", 904);
-}
